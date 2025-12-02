a70a79436256251c7b28a4dbd260026e9f609d83
Jean-Éric Bisson <jebisson@ressources.coop>
2025-12-02 08:47:29 -0500

    Prepare for Vercel deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,7 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
-import vercel from "@astrojs/vercel/serverless"; // Node runtime (required for Nodemailer)
-import tailwind from '@astrojs/tailwind';
-// If you're using React components, also add:
-// import react from '@astrojs/react';
+import { defineConfig } from "astro/config";
+import vercel from "@astrojs/vercel/serverless";
 
 export default defineConfig({
-  site: 'https://jebisson.github.io/EESAD',
-  base: '/EESAD',
-  integrations: [
-    tailwind({ applyBaseStyles: true }),
-    // react(),
-  ],
-});
-
-export default defineConfig({
-  adapter: vercel(),
-  output: "server",  // enables /api routes
+  adapter: vercel(),  // makes /api/submit work on Vercel
+  output: "server",   // enables server routes
 });
